27ec1e01e0c3da34c3d85063ee0443e0227eba1f
Rui Carneiro <rui.motacarneiro@heig-vd.ch>
2022-12-15 11:23:48 +0100

    to test

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,4 +1,26 @@
 var Chance = require('chance');
 var chance = new Chance();
+var express = require('express');
+var app = express();
 
-console.log("Random password : " + chance.string());
+app.get('/', function(req, res) {
+	res.send( generatePasswords() );
+});
+
+app.listen(7777, function () {
+	console.log('Accepting HTTP requests on port 7777.');
+});
+
+function generatePasswords() {
+	var numberOfPasswords = chance.integer({
+		min: 1,
+		max: 10
+	});
+	console.log(numberOfPasswords);
+	var passwords = [];
+	for (var i = 0; i < numberOfPasswords; i++) {
+		passwords.push(chance.string());
+	}
+	console.log(passwords);
+	return passwords;
+}
